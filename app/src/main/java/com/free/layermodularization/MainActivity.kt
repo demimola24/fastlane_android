50f7b15029c0dfb42df5c93a6f0afdb9e968c862
demimola24 <trideks0331>
2020-11-01 00:43:29 +0100

    new dummy feature sent for review

--- app/src/main/java/com/free/layermodularization/MainActivity.kt
+++ app/src/main/java/com/free/layermodularization/MainActivity.kt
@@ -31,7 +31,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //CoreApplication.coreComponent(this@MainActivity).inject(this@MainActivity)
         MainInjector.init(this)
 
         setContentView(R.layout.activity_main)
